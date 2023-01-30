c331bcc69b2de581f14583b23184c7ca28c5cfee
Reinitialized <me@reinitialized.studio>
2023-01-29 19:57:25 -0600

    moved autologin logic to beginning to help diagnose issues

--- Install.ps1
+++ Install.ps1
@@ -1,5 +1,15 @@
 Start-Transcript -Append "C:\AutoDeploy\Logs\Install.log"
 
+Write-Output "Configuring Local Device Administrator"
+$LocalUser = New-LocalUser -Name "simpatico" -FullName "Simpatico Systems, LLC" -Description "Local Device Administrator" -Password (ConvertTo-SecureString -String "2Rbsx931nKXKye2D" -AsPlainText -Force)
+Set-LocalUser -Name "simpatico" -AccountNeverExpires -PasswordNeverExpires
+Add-LocalGroupMember -Group (Get-LocalGroup -Name "Administrators") -Member $LocalUser
+
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name AutoAdminLogin -Value 1 -Force
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name DefaultDomainName -Value . -Force
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name DefaultUserName -Value "simpatico" -Force
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name DefaultPassword -Value "2Rbsx931nKXKye2D" -Force
+
 $USBDrive = Get-WmiObject Win32_LogicalDisk -Filter "DriveType = 2" | Where-Object { $_.VolumeName -eq "AutoDeployUSB" }
 if (-Not $USBDrive) {
     Throw "Cannot continue: Unable to locate AutoDeploy USB, a required dependency. Please try again."
@@ -33,16 +43,6 @@ Copy-Item -Path "$($USBDrive.DeviceId)\AutoDeploy\Applications" -Destination "C:
 
 Write-Output "Downloading Setup.ps1"
 Invoke-WebRequest "https://raw.githubusercontent.com/Reinitialized/AutoDeploy/indev/Setup.ps1" -OutFile C:\AutoDeploy\Setup.ps1
-
-Write-Output "Configuring Local Device Administrator"
-$LocalUser = New-LocalUser -Name "simpatico" -FullName "Simpatico Systems, LLC" -Description "Local Device Administrator" -Password (ConvertTo-SecureString -String "2Rbsx931nKXKye2D" -AsPlainText -Force)
-Set-LocalUser -Name "simpatico" -AccountNeverExpires -PasswordNeverExpires
-Add-LocalGroupMember -Group (Get-LocalGroup -Name "Administrators") -Member $LocalUser
-
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name AutoAdminLogin -Value 1 -Force
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name DefaultDomainName -Value . -Force
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name DefaultUserName -Value "simpatico" -Force
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -PropertyType String -Name DefaultPassword -Value "2Rbsx931nKXKye2D" -Force
 
 Write-Output "Setting AutoDeploy to run on first login"
 New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -PropertyType String -Name "AutoDeploy" -Value "PowerShell -ExecutionPolicy Bypass -File C:\AutoDeploy\Setup.ps1" -Force
